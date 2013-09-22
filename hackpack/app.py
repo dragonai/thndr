042cb82f08b11c6a22e1ea016ed8a2f321fdbf97
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2013-09-22 00:19:37 -0400

    Tweaked voice request

--- hackpack/app.py
+++ hackpack/app.py
@@ -16,8 +16,7 @@ app.config.from_pyfile('local_settings.py')
 @app.route('/voice', methods=['GET', 'POST'])
 def voice():
     response = twiml.Response()
-    response.say("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    response.say("Hi there. Please shoot us a text instead.")
     return str(response)
 
 
